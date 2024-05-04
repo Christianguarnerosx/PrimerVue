5c62956e7436d1a080447af5a74b822cc529e1d5
Christianguarnerosx <christianguarnerosx@gmail.com>
2024-05-03 22:46:00 -0600

    Crear proyecto vue

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+/* Single file component */
+//Logica
+<script setup>
+const name = 'Vue Dinamico';
+
+//metodo - methods
+
+const hacerclick = (mensaje) =>{
+    console.log(mensaje)
+}
+</script>
+
+//Estructura
+//(template obligatorio)
+<template>
+    <!-- Bien, lo que vemos es -->
+<button v-on:click="hacerclick('Hola opresor 1')">Presioname</button>
+<button @:click="hacerclick('Hola crack 2')">Presioname 2</button>
+</template>
+
+//Estilos
+<style>
+h1{
+    color: red;
+}
+</style>
